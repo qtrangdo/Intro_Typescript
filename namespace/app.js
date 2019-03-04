835beae7bd9074a2bdb00212f411e7140c41ba3e
td94 <quynhtrangdo94@gmail.com>
2019-03-04 00:45:28 -0800

    Work with namespace

---
+++ namespace/app.js
@@ -0,0 +1,26 @@
+var MyMath;
+(function (MyMath) {
+    var Circle;
+    (function (Circle) {
+        var PI = 3.14;
+        function calculateCircumference(diameter) {
+            return diameter * PI;
+        }
+        Circle.calculateCircumference = calculateCircumference;
+        ;
+    })(Circle = MyMath.Circle || (MyMath.Circle = {}));
+})(MyMath || (MyMath = {}));
+var MyMath;
+(function (MyMath) {
+    function calculateRectangle(width, length) {
+        return width * length;
+    }
+    MyMath.calculateRectangle = calculateRectangle;
+})(MyMath || (MyMath = {}));
+/// <reference path="circleMath.ts" />
+/// <reference path="rectangleMath.ts" />
+var circleMath = MyMath.Circle;
+var PI = 2.99;
+console.log(MyMath.calculateRectangle(10, 20));
+console.log(circleMath.calculateCircumference(3));
+console.log(PI);
