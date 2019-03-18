70f14588e981da070aecbef26458a203efec5e85
td94 <quynhtrangdo94@gmail.com>
2019-03-17 21:21:11 -0700

    Load module with SystemJS

---
+++ module/app.js
@@ -0,0 +1,8 @@
+define(["require", "exports", "./math/circle"], function (require, exports, circle_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    // import {} from './math/rectangle';
+    console.log(circle_1.PI);
+    console.log(circle_1.calculateCircumference(2));
+});
+// console.log();
---
+++ module/app.ts
@@ -0,0 +1,6 @@
+import {PI, calculateCircumference} from './math/circle';
+// import {} from './math/rectangle';
+
+console.log(PI);
+console.log(calculateCircumference(2));
+// console.log();
