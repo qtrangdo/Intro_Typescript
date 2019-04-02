e50396629093cacc7b0e6b3e6320fc6c073543a9
td94 <quynhtrangdo94@gmail.com>
2019-04-02 18:45:05 -0400

    Add simple increase/decrease buttons

--- ts_with_react/src/App.tsx
+++ ts_with_react/src/App.tsx
@@ -1,13 +1,36 @@
 import * as React from 'react';
 import CounterOutput from './CounterOutput';
 
+// interface IAppProps {
 
-class App extends React.Component {
+// }
+
+interface IAppState {
+  counterValue: number;
+}
+
+class App extends React.Component<{}, IAppState> {
+  public state = {counterValue: 0};
+  
   public render() {
     return (
-      <CounterOutput counter={1} />
+      <div style={{textAlign: 'center'}}>
+        <CounterOutput counter={this.state.counterValue} />
+        <button onClick={this.incHandler}>Increment</button>
+        <button onClick={this.decHandler}>Decrement</button>
+      </div>
     );
   }
+  
+  private incHandler = () => {
+    const newcounter = this.state.counterValue + 1;
+    this.setState({ counterValue: newcounter})
+  };
+
+  private decHandler = () => {
+    const newcounter = this.state.counterValue - 1;
+    this.setState({ counterValue: newcounter})
+  };
 }
 
 export default App;
